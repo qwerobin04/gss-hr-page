a1832aa93662c4d1898b0b50ef4c6eb1db83336c
Robin <abanesjohnrobin@gmail.com>
2018-07-19 17:45:26 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,166 @@
+$(document).ready(function(){
+  //ANCHOR
+  $("a").on('click', function(event) {
+
+     // Make sure this.hash has a value before overriding default behavior
+     if (this.hash !== "") {
+       // Prevent default anchor click behavior
+       event.preventDefault();
+
+       // Store hash
+       var hash = this.hash;
+
+       // Using jQuery's animate() method to add smooth page scroll
+       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 800, function(){
+
+         // Add hash (#) to URL when done scrolling (default click behavior)
+         window.location.hash = hash;
+       });
+     } // End if
+   });
+
+    $(".cp-nav-text_1").click(function(){
+        $(".cp-nav-link").removeClass("is-active");
+        $(this).addClass("is-active");
+        $(".cp-slider").removeClass("is-active");
+        $(".slider-container_1").addClass("is-active");
+        $(".slider-container_1").get(0).slick.setPosition();
+
+
+    });
+
+    $(".cp-nav-text_2").click(function(){
+        $(".cp-nav-link").removeClass("is-active");
+        $(this).addClass("is-active");
+        $(".cp-slider").removeClass("is-active");
+        $(".slider-container_2").addClass("is-active");
+        $(".slider-container_2").get(0).slick.setPosition();
+    });
+
+    $(".cp-nav-text_3").click(function(){
+        $(".cp-nav-link").removeClass("is-active");
+        $(this).addClass("is-active");
+        $(".cp-slider").removeClass("is-active");
+        $(".slider-container_3").addClass("is-active");
+        $(".slider-container_3").get(0).slick.setPosition();
+    });
+
+    $(".cp-nav-text_4").click(function(){
+        $(".cp-nav-link").removeClass("is-active");
+        $(this).addClass("is-active");
+        $(".cp-slider").removeClass("is-active");
+        $(".slider-container_4").addClass("is-active");
+        $(".slider-container_4").get(0).slick.setPosition();
+    });
+
+    //GSS MAPS
+    $("#gss-map_ph").click(function(){
+      $(".gss-ph-iframe").removeClass("hide");
+      $(".gss-jp-iframe").addClass("hide");
+    });
+    $("#gss-map_jp").click(function(){
+      $(".gss-jp-iframe").removeClass("hide");
+      $(".gss-jp-iframe").addClass("show");
+      $(".gss-ph-iframe").addClass("hide");
+    });
+
+
+  });
+
+    $('.slider-nav').slick({
+     slidesToShow: 4,
+     slidesToScroll: 1,
+     speed: 300,
+     dots: false,
+     focusOnSelect: true
+    });
+
+    // CAREER PATH - SLIDER
+    $('.slider-container_1').slick({
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         dots: false,
+         infinite: false,
+         speed: 300,
+         fade: true,
+         focusOnSelect: true
+    });
+    $('.slider-container_2').slick({
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         dots: false,
+         infinite: false,
+         speed: 300,
+         fade: true,
+         focusOnSelect: true
+    });
+    $('.slider-container_3').slick({
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         dots: false,
+         infinite: false,
+         speed: 300,
+         fade: true,
+         focusOnSelect: true
+    });
+    $('.slider-container_4').slick({
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         dots: false,
+         infinite: false,
+         speed: 300,
+         fade: true,
+         focusOnSelect: true
+    });
+
+
+
+//
+//     $('.car-path-slider_1').slick({
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       dots: false,
+//       infinite: false,
+//       speed: 300,
+//       fade: true,
+//       focusOnSelect: true,
+//       cssEase: 'linear'
+//     });
+// });
+//
+// $('.car-path-slider_2').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   dots: false,
+//   infinite: false,
+//   speed: 300,
+//   fade: true,
+//   focusOnSelect: true,
+//   cssEase: 'linear'
+// });
+//
+// $('.car-path-slider_3').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   dots: false,
+//   infinite: false,
+//   speed: 300,
+//   fade: true,
+//   centerMode: true,
+//   focusOnSelect: true,
+//   cssEase: 'linear'
+// });
+// $('.car-path-slider_4').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   dots: false,
+//   infinite: false,
+//   speed: 300,
+//   fade: true,
+//   centerMode: true,
+//   focusOnSelect: true,
+//   cssEase: 'linear'
+// });
