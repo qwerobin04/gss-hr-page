b31c2dcc98c65c8846c575ab486239e808efa72b
Robin <abanesjohnrobin@gmail.com>
2018-08-06 16:38:25 +0800

    animation changes

--- js/script.js
+++ js/script.js
@@ -185,49 +185,94 @@ $(document).ready(function(){
 
 $(window).on('resize scroll', function() {
 if ($('.h-wwaat').isInViewport()) {
+  $( ".h-waat" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.h-wwaat').addClass('animated fadeInUp');
 }
 
 if ($('.h-content1').isInViewport()) {
+  $( ".h-content1" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.h-content1').addClass('animated fadeInUp');
 }
 if ($('.gallery-container').isInViewport()) {
+  $( ".gallery-container" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.gallery-container').addClass('animated fadeInUp');
 }
 if ($('.eb').isInViewport()) {
+  $( ".eb" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.eb').addClass('animated fadeInUp');
 }
 if ($('.eb-icons').isInViewport()) {
+  $( ".eb-icons" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.eb-icons').addClass('animated fadeInUp');
 }
 if ($('.tech-title').isInViewport()) {
+  $( ".tech-title" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.tech-title').addClass('animated fadeInUp');
 }
 if ($('.tech-text').isInViewport()) {
+  $( ".tech-text" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.tech-text').addClass('animated fadeInUp');
 }
-if ($('.tech-img').isInViewport()) {
-  $('.tech-img').addClass('animated fadeInUp');
+if ($('.tech-icons').isInViewport()) {
+  $( ".tech-icons" ).animate({
+    opacity: [1]
+    }, 300 );
+  $('.tech-icons').addClass('animated fadeInUp');
 }
 if ($('.car-path-title').isInViewport()) {
+  $( "car-path-title" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.car-path-title').addClass('animated fadeInUp');
 }
 if ($('.car-path-menu').isInViewport()) {
+  $( ".car-path-menu" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.car-path-menu').addClass('animated fadeInUp');
 }
 if ($('.cp-slider').isInViewport()) {
+  $( ".cp-slider" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.cp-slider').addClass('animated fadeInUp');
 }
 if ($('.serv-h2').isInViewport()) {
+  $( ".serv-h2" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.serv-h2').addClass('animated fadeInUp');
 }
 if ($('.serv-p').isInViewport()) {
+  $( ".serv-p" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.serv-p').addClass('animated fadeInUp');
 }
 if ($('.s-icons1').isInViewport()) {
+  $( ".s-icons1" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.s-icons1').addClass('animated fadeInUp');
 }
 if ($('.s-icons2').isInViewport()) {
+  $( ".s-icons2" ).animate({
+    opacity: [1]
+    }, 300 );
   $('.s-icons2').addClass('animated fadeInUp');
 }
 if ($('.connect-p').isInViewport()) {
